1234ed09ecaa2bbca970878c472fc41109fbf8fe
Dave Alden <dpa99c@gmail.com>
2017-04-05 14:19:25 +0100

    Update to test load events with WKWebView

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,37 @@
-var testUrl = 'http://www.plupload.com/examples';
+var testUrl = 'http://www.google.co.uk';
+var outputEl;
+
+function log(msg){
+    console.log(msg);
+    logToPage(msg);
+}
+
+function warn(){
+    console.warn(msg);
+    logToPage(msg);
+}
+
+function error(){
+    console.error(msg);
+    logToPage(msg);
+}
+
+function logToPage(msg){
+    outputEl.innerHTML += '<p>' + msg + '</p>';
+}
+
+function onLoadEvent(name, browserName) {
+    log(browserName + " load event: " + name);
+}
+
+function addLoadEventListeners(iab, browserName){
+    iab.addEventListener("loadstart", onLoadEvent.bind(this, "loadstart", browserName));
+    iab.addEventListener("loadstop", onLoadEvent.bind(this, "loadstop", browserName));
+    iab.addEventListener("loaderror", onLoadEvent.bind(this, "loaderror", browserName));
+}
 
 function openThemeableBrowser(){
-    cordova.ThemeableBrowser.open(testUrl, '_blank', {
+    var iab = cordova.ThemeableBrowser.open(testUrl, '_blank', {
         statusbar: {
             color: '#ffffffff'
         },
@@ -13,25 +43,36 @@ function openThemeableBrowser(){
             color: '#003264ff',
             showPageTitle: true
         },
+        backButton: {
+            wwwImage: 'img/chevron-left.png',
+            wwwImageDensity: 4,
+            imagePressed: 'back_pressed',
+            align: 'left',
+            event: 'backPressed'
+        },
+        closeButton: {
+            wwwImage: 'img/cross-mark-on-a-black-circle-background.png',
+            wwwImageDensity: 4,
+            imagePressed: 'close_pressed',
+            align: 'right',
+            event: 'closePressed'
+        },
         backButtonCanClose: true
-    }).addEventListener('backPressed', function(e) {
-        console.log('back pressed');
-    }).addEventListener('helloPressed', function(e) {
-        console.log('hello pressed');
-    }).addEventListener('sharePressed', function(e) {
-        console.log(e.url);
     }).addEventListener(cordova.ThemeableBrowser.EVT_ERR, function(e) {
-        console.error(e.message);
+        error(e.message);
     }).addEventListener(cordova.ThemeableBrowser.EVT_WRN, function(e) {
-        console.warn(e.message);
+        warn(e.message);
     });
+    addLoadEventListeners(iab, "themable");
 }
 
 function openInAppBrowser(){
-    cordova.InAppBrowser.open(testUrl, '_blank', 'location=yes');
+    var iab = cordova.InAppBrowser.open(testUrl, '_blank', 'location=yes');
+    addLoadEventListeners(iab, "iab");
 }
 
 function onDeviceReady(){
-    console.log("deviceready");
+    outputEl = document.getElementById('output');
+    log("deviceready");
 }
 document.addEventListener('deviceready', onDeviceReady, false);
