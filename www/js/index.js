a83a4786716f73c7a18eb019d77d302b77883eea
Dave Alden <dpa99c@gmail.com>
2017-11-21 16:16:12 +0000

    Configure to test android_close_button branch

--- www/js/index.js
+++ www/js/index.js
@@ -1,34 +1,7 @@
-var testUrl = 'http://www.plupload.com/examples';
-
-function openThemeableBrowser(){
-    cordova.ThemeableBrowser.open(testUrl, '_blank', {
-        statusbar: {
-            color: '#ffffffff'
-        },
-        toolbar: {
-            height: 44,
-            color: '#f0f0f0ff'
-        },
-        title: {
-            color: '#003264ff',
-            showPageTitle: true
-        },
-        backButtonCanClose: true
-    }).addEventListener('backPressed', function(e) {
-        console.log('back pressed');
-    }).addEventListener('helloPressed', function(e) {
-        console.log('hello pressed');
-    }).addEventListener('sharePressed', function(e) {
-        console.log(e.url);
-    }).addEventListener(cordova.ThemeableBrowser.EVT_ERR, function(e) {
-        console.error(e.message);
-    }).addEventListener(cordova.ThemeableBrowser.EVT_WRN, function(e) {
-        console.warn(e.message);
-    });
-}
+var testUrl = 'http://www.bbc.co.uk/news';
 
 function openInAppBrowser(){
-    cordova.InAppBrowser.open(testUrl, '_blank', 'location=yes');
+    cordova.InAppBrowser.open(testUrl, '_blank', 'location=no,footer=yes', null, {footerText: "Custom text"});
 }
 
 function onDeviceReady(){
